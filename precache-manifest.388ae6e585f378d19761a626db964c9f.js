ca243e86d0f425d54f7459df4591aef9447722c1
Joseph Idbensalah <joseph.idbensalah@gmail.com>
2020-09-04 01:39:22 +0100

    Updates

---
+++ precache-manifest.388ae6e585f378d19761a626db964c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59c11fedc2d146aa793f335e315b144",
+    "url": "/To-do-List-App-ReactJs/index.html"
+  },
+  {
+    "revision": "e620bb9310592c87d16a",
+    "url": "/To-do-List-App-ReactJs/static/css/main.fe883c76.chunk.css"
+  },
+  {
+    "revision": "6b7f8c51baaf69eb0110",
+    "url": "/To-do-List-App-ReactJs/static/js/2.460f16f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-List-App-ReactJs/static/js/2.460f16f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e620bb9310592c87d16a",
+    "url": "/To-do-List-App-ReactJs/static/js/main.8169901a.chunk.js"
+  },
+  {
+    "revision": "2fead4fd485cdded92fb",
+    "url": "/To-do-List-App-ReactJs/static/js/runtime-main.0caedeeb.js"
+  }
+]);
